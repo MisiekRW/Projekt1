802a3ebb309599edd60508a7140eaebde68d32b5
Michał Dziwisz <michalrw6@o2.pl>
2017-09-16 13:28:52 +0200

    wyleworwerowany leworweowiec

---
+++ ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Projekt1
+//
+//  Created by Michał Dziwisz on 16.09.2017.
+//  Copyright © 2017 Michał Dziwisz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("siema ziomuś")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
