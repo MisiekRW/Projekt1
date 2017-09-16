6cc3ba9e077edf6114df2567597489b86191f276
Michał Dziwisz <michalrw6@o2.pl>
2017-09-16 13:47:06 +0200

    Print v2

--- ViewController.swift
+++ ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("siema ziomuś")
+        print("Witaj Świecie")
     }
 
     override func didReceiveMemoryWarning() {
